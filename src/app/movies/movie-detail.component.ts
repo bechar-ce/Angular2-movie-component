2bf0c5b4c5c4b529a61fe432d914699101c729e4
ccetkemche <bechar_ce@rediffmail.com>
2017-03-26 21:45:59 +0530

    Movie Detail component - Done

--- src/app/movies/movie-detail.component.ts
+++ src/app/movies/movie-detail.component.ts
@@ -1,28 +1,40 @@
 import {Component, OnInit} from '@angular/core';
 import {Router, ActivatedRoute} from "@angular/router";
+import {MovieService} from "./movie.service";
+import {IMovie} from "./movie";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'app-movie-detail',
-  templateUrl: './movie-detail.component.html',
+    selector: 'app-movie-detail',
+    templateUrl: './movie-detail.component.html',
 })
 export class MovieDetailComponent implements OnInit {
 
-  pageTitle: string = 'Movie Detail';
+    pageTitle: string = 'Movie Detail';
+    movie: IMovie;
+    private sub: Subscription;
+    errorMessage: string;
 
-  constructor(private _router: Router, private _routeParams: ActivatedRoute) {
-    this._routeParams.params.subscribe(params => {
-      let id = Number.parseInt(params['id']);
-      console.log("id::: " + id);
-      this.pageTitle += `: ${id}`;
-    });
-  }
+    constructor(private _router: Router, private _routeParams: ActivatedRoute, private _movieService: MovieService) {
 
-  ngOnInit() {
+    }
 
-  }
+    ngOnInit() {
+        this._routeParams.params.subscribe(params => {
+            let id = Number.parseInt(params['id']);
+            console.log("id::: " + id);
+            this.getProduct(id);
+        });
+    }
 
-  onBack(): void {
-    this._router.navigate(['movie-list']);
-  }
+    getProduct(id: number) {
+        this._movieService.getMovie(id).subscribe(
+            movie => this.movie = movie,
+            error => this.errorMessage = <any>error);
+    }
+
+    onBack(): void {
+        this._router.navigate(['movie-list']);
+    }
 
 }
--- src/app/movies/movie.service.ts
+++ src/app/movies/movie.service.ts
@@ -6,21 +6,26 @@ import {Observable} from "rxjs";
 @Injectable()
 export class MovieService {
 
-  private _movieUrl = '../api/movies/movies.json';
+    private _movieUrl = '../api/movies/movies.json';
 
-  constructor(private _http: Http) {
+    constructor(private _http: Http) {
 
-  }
+    }
 
-  getMovies(): Observable<IMovie[]> {
-    return this._http.get(this._movieUrl)
-      .map((response: Response)=><IMovie[]>response.json())
-      .do(data => console.log("All: " +  JSON.stringify(data)))
-      .catch(this.handleError);
-  }
+    getMovies(): Observable<IMovie[]> {
+        return this._http.get(this._movieUrl)
+            .map((response: Response)=><IMovie[]>response.json())
+            .do(data => console.log("All: " + JSON.stringify(data)))
+            .catch(this.handleError);
+    }
 
-  private handleError(error: Response) {
-    console.log("Error:: " + error);
-    return Observable.throw(error.json().error || "Server Error")
-  }
+    getMovie(id: number): Observable<IMovie> {
+        return this.getMovies()
+            .map((movies: IMovie[]) => movies.find(m => m.movieID === id));
+    }
+
+    private handleError(error: Response) {
+        console.log("Error:: " + error);
+        return Observable.throw(error.json().error || "Server Error")
+    }
 }
